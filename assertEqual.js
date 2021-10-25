92d42c08a2f61df2dda8b6ecdbae488835f2a270
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-10-25 23:26:34 +0000

    Initial Commit

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`⛔️ 😭 ⛔️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅ 😎 ✅Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("bootcamp", "Bootcamp");
+assertEqual(4, 6);
