23cdef4819cf3bc1b6d89f88f3bb70b9b169edfc
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-10-27 22:35:20 +0000

    Add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+//Assertion Function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    let ans = true;
+    for (let i in arr1) {
+      if (arr1[i] !== arr2[i]) {
+        ans = false;
+      }
+    }
+    return ans;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log('✅ 😎 ✅ Assertion Passed: ', array1, ' === ', array2);
+  } else {
+    console.log('⛔️ 😭 ⛔️ Assertion Failed: ', array1, ' !== ', array2);
+  }
+};
+
+//LetterPositions Function
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    (results[sentence[i]]) ? results[sentence[i]].push(i) : results[sentence[i]] = [i];
+  }
+  return results;
+};
+
+//TEST Functions
+assertArraysEqual(letterPositions('hello')['h'], [0]);
+assertArraysEqual(letterPositions('hello')['e'], [1]);
+assertArraysEqual(letterPositions('hello')['l'], [2, 3]);
+assertArraysEqual(letterPositions('hello')['o'], [4]);
