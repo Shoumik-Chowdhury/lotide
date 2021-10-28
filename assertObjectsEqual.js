b16f70417e36074ec3bebee41a5a77c4758b7351
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-10-28 03:16:15 +0000

    Add assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,57 @@
+//eqArray function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    let ans = true;
+    for (let i in arr1) {
+      if (arr1[i] !== arr2[i]) {
+        ans = false;
+      }
+    }
+    return ans;
+  } else {
+    return false;
+  }
+};
+
+//eqObjects Function
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//assertObjectsEqual function
+const assertObjectsEqual = function(obj1, obj2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(obj1, obj2)) {
+    console.log(`✅ 😎 ✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`⛔️ 😭 ⛔️ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
+  }
+};
+
+//Tets Function
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
+
+//
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false
