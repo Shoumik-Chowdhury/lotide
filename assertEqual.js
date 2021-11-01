26241cdbac952d00c29762e668f81d408c1e571d
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-01 18:04:08 +0000

    refactor head.js and assertEqual.js and add test

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("bootcamp", "Bootcamp");
-assertEqual(4, 6);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ 😎 ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️ 😭 ⛔️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-//TEST CASE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([6]), 6);
+module.exports = head;
